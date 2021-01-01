3c3570299c7901f793a1ac281f968a7ca3761303
scottsam <samuel_owulezi@yahoo.com>
2021-01-01 02:54:57 +0100

    Updates

---
+++ precache-manifest.17909eb62edb58c0ed64539c324cd2c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6efc2b564cfec538129179005977cd",
+    "url": "/super/index.html"
+  },
+  {
+    "revision": "7160a5f7776f2660236d",
+    "url": "/super/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "70db764affd710f1f020",
+    "url": "/super/static/css/main.ae2b79ef.chunk.css"
+  },
+  {
+    "revision": "7160a5f7776f2660236d",
+    "url": "/super/static/js/2.0c635b25.chunk.js"
+  },
+  {
+    "revision": "68151e78c5821b8c4c18",
+    "url": "/super/static/js/3.e9846c97.chunk.js"
+  },
+  {
+    "revision": "70db764affd710f1f020",
+    "url": "/super/static/js/main.63d7a1c0.chunk.js"
+  },
+  {
+    "revision": "3f9c25f6e562bead0e22",
+    "url": "/super/static/js/runtime~main.bb5c2d60.js"
+  }
+]);
